1f5295e2d4b0bfd94479cc6a1622eeb1e67db3ed
GitEulerAngles <62534632+GitEulerAngles@users.noreply.github.com>
2024-01-24 18:29:07 -0600

    Create main.cpp

---
+++ V 0.1/Sources Files/main.cpp
@@ -0,0 +1,47 @@
+#include "bot.h"
+#include "window.hpp"
+#include "basic.h"
+#include <thread>
+#include <iostream>
+
+#undef main
+
+void main() {
+    Bot b[AMOUNT];
+    window* w[AMOUNT];
+
+    std::cout << "Loading Setup!\n";
+    for (int i = 0; i < AMOUNT; i++) {
+        b[i].setup();
+        w[i] = new window("Game", (i % 12) * 160, floor(i / 12) * 350 + 32, 160, 320, 60);
+        w[i]->setup(&b[i]);
+    }
+
+    b[0].neuralNetwork.load();
+    std::cout << "Setup Complete!\n";
+
+    float frameDelay = 1000 / w[0]->FPS;
+    float frameTime;
+
+    while (w[0]->running) {
+        for (int i = 0; i < AMOUNT; i++) {
+            if (b[i].tetrisGame.gameOver)
+                continue;
+
+            w[i]->input(&b[i]);
+            w[i]->update(&b[i]);
+            w[i]->render(&b[i]);
+        }
+
+        bool allGameOver = true;
+        for (int i = 0; i < AMOUNT; i++)
+            allGameOver = allGameOver && b[i].tetrisGame.gameOver;
+
+        if (allGameOver) {
+            trainBots(b);
+        }
+    }
+
+    for (int i = 0; i < AMOUNT; i++)
+        w[i]->clean(&b[i]);
+}
