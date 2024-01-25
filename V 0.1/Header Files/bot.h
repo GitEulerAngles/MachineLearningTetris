e9aea4ef53010abbedfad7878f38438603a3cb30
GitEulerAngles <62534632+GitEulerAngles@users.noreply.github.com>
2024-01-24 18:30:53 -0600

    Create bot.h

---
+++ V 0.1/Header Files/bot.h
@@ -0,0 +1,23 @@
+#ifndef BOT
+#define BOT
+
+#include "tetris.h"
+#include "neural_network.h"
+
+#define AMOUNT 18
+
+class Bot{
+private:
+public:
+    float grid[20][10];
+    Tetris tetrisGame;
+    NeuralNetwork neuralNetwork;
+    basic::texture texture;
+    void generateGrid();
+    void setup();
+    void update();
+};
+
+void trainBots(Bot(&botArr)[AMOUNT]);
+
+#endif //!BOT
